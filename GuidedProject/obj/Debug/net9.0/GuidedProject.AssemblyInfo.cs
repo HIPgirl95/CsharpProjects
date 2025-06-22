60a1eefa01e46b21a44e9a8ff30d1165ff97676d
HIPgirl95 <hannah@dawne.com>
2025-06-22 15:37:51 -0400

    Revert "adding files to gitignore"
    This reverts commit 9df19c7b206604c20401bbf350992482ad83c973.

--- GuidedProject/obj/Debug/net9.0/GuidedProject.AssemblyInfo.cs
+++ GuidedProject/obj/Debug/net9.0/GuidedProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GuidedProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7afa249ac511cf7825f80a2652854e0eace3bfbd")]
 [assembly: System.Reflection.AssemblyProductAttribute("GuidedProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GuidedProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
