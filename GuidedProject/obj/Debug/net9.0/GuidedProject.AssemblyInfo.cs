da802f8122423505b23f6923580afdc215237d6d
HIPgirl95 <hannah@dawne.com>
2025-06-22 15:42:19 -0400

    build and run

--- GuidedProject/obj/Debug/net9.0/GuidedProject.AssemblyInfo.cs
+++ GuidedProject/obj/Debug/net9.0/GuidedProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GuidedProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7afa249ac511cf7825f80a2652854e0eace3bfbd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89522b2cc7c07736a38603d5e8f231d1a7dee8b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("GuidedProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GuidedProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
